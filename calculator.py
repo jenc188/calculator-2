70288d22a184c2a44c4e1b14749ab6949fa50bad
Jenny C <jennc388@gmail.com>
2021-12-02 04:40:36 +0000

    Add initial codes

--- calculator.py
+++ calculator.py
@@ -5,3 +5,36 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 
 
 # Replace this with your code
+while True:
+    user_input = input("Enter your equation> ")
+    token = user_input.split(' ')
+    operator = token[0]
+    result = None
+    num1 = token[1]
+    num2 = token[2]
+    if operator == "q":
+        break
+    elif len(token)<2:
+        print("It is not enough input")
+    else:
+        if operator == "+":
+            result = add(num1,num2)
+        elif operator == "-":
+            result = subtract(num1, num2)
+        
+        elif operator == "*":
+            result = multiply(num1, num2)
+        
+        elif operator == "/":
+            result = divide(num1, num2)
+        
+        elif operator == "*":
+            result = square(num1)
+        
+        elif operator == "**":
+            result = cube(num1)
+        
+        elif operator == "**=":
+            result = cube(num1, num2)
+            
+print(result)
